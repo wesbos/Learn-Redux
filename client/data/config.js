354e919981c8e7b23b032f3b43ee97934c784890
wesbos <wesbos@gmail.com>
2016-01-10 19:19:44 -0500

    redid tokens to read-only

--- client/data/config.js
+++ client/data/config.js
@@ -1,3 +1,4 @@
 const SENTRY_KEY = 'cb55d4f05cd443ce82303222f77ef5e0';
 const SENTRY_APP = '61499';
 export const SENTRY_URL = `${SENTRY_KEY}@app.getsentry.com/${SENTRY_APP}`;
+export const instagramToken = '519208.c7a5a3e.53aeb7c485cb460cbab698d44118b809';
--- client/data/endpoints.js
+++ client/data/endpoints.js
@@ -1,11 +1,11 @@
 const base = 'https://api.instagram.com/v1';
-const accessToken = '519208.e1ed559.84ef4e75730249e48731701c3d89fa29';
+import { instagramToken } from './config';
 
 export function commentEndpoint(id) {
-  return `${base}/media/${id}/comments?access_token=${accessToken}&callback=lol`;
+  return `${base}/media/${id}/comments?access_token=${instagramToken}&callback=lol`;
 }
 
 export function mediaEndpoint(userId = 519208) {
-  return `${base}/users/${userId}/media/recent?access_token=${accessToken}&callback=lol`;
+  return `${base}/users/${userId}/media/recent?access_token=${instagramToken}&callback=lol`;
 }
 
